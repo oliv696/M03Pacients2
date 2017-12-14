145d2cec053821be9854e556b399d081102d38c3
TheJordisc <jsolaceada@gmail.com>
2017-12-14 14:09:30 +0100

    -Faltaba quitar un TODO :/

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,7 @@ public class Main {
 
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException {
-
-		//TODO: Revisar - java.util.InputMismatchException al meter doubles
-
 		Program p=new Program();
-
 		p.menuP();
 	}
 
